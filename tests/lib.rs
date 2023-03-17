cc47a4be64e399a98b5a716a3b541987e9d9f643
j6k1 <nanasi18@gmail.com>
2023-03-18 08:03:30 +0900

    Add test case

--- tests/lib.rs
+++ tests/lib.rs
@@ -51,7 +51,6 @@ fn small_2d() {
         kdt.insert(pn,());
     }
 
-    //dbg!(&kdt);
     let mut success = 0;
 
     for _ in 0..COUNT {
@@ -70,4 +69,228 @@ fn small_2d() {
     }
 
     println!("正答率 {}%",success as f64 / COUNT as f64 * 100.);
+}
+#[test]
+fn small_3d() {
+    const COUNT:usize = 100;
+
+    let mut kdt = KDTree::<3,f64,()>::new();
+
+    let mut rng = rand::thread_rng();
+
+    let mut positions:Vec<[f64;3]> = vec![];
+
+    for _ in 0..COUNT {
+        let mut pn = [0f64;3];
+
+        for i in 0..3 {
+            let mut p:f64 = rng.gen_range(-100000.0..100000.0);
+            let pi:u8 = rng.gen_range(0..10);
+
+            if let Some(last) = positions.last() {
+                if pi == 0 {
+                    p = last[i];
+                }
+            }
+
+            pn[i] = p;
+        }
+
+        positions.push(pn.clone());
+        kdt.insert(pn,());
+    }
+
+    let mut success = 0;
+
+    for _ in 0..COUNT {
+        let x:f64 = rng.gen_range(-100000.0..100000.0);
+        let y:f64 = rng.gen_range(-100000.0..100000.0);
+        let z:f64 = rng.gen_range(-100000.0..100000.0);
+
+        let p = [x,y,z];
+        let expected = nearest(&p,&positions);
+        let actual = kdt.nearest_position(&p);
+
+        if (p.euclidean_distance(expected) - p.euclidean_distance(&actual.unwrap())) < 1. {
+            success += 1;
+        } else {
+            dbg!(expected,actual.unwrap(),p.euclidean_distance(expected),p.euclidean_distance(&actual.unwrap()));
+        }
+    }
+
+    println!("正答率 {}%",success as f64 / COUNT as f64 * 100.);
+}
+#[test]
+fn small_2d_unbalance() {
+    const COUNT:usize = 100;
+
+    let mut kdt = KDTree::<2,f64,()>::new();
+
+    let mut rng = rand::thread_rng();
+
+    let mut positions:Vec<[f64;2]> = vec![];
+
+    for _ in 0..COUNT {
+        let mut pn = [0f64;2];
+
+        for i in 0..2 {
+            let mut p:f64 = rng.gen_range(-100000.0..100000.0);
+            let pi:u8 = rng.gen_range(0..10);
+
+            if let Some(last) = positions.last() {
+                if pi == 0 {
+                    p = last[i];
+                }
+            }
+
+            pn[i] = p;
+        }
+
+        positions.push(pn.clone());
+    }
+
+    positions.sort_by(|a,b| a[0].partial_cmp(&b[0]).unwrap().then(a[1].partial_cmp(&b[1]).unwrap()) );
+
+    for p in &positions {
+        kdt.insert(p.clone(),());
+    }
+
+    let mut success = 0;
+
+    for _ in 0..COUNT {
+        let x:f64 = rng.gen_range(-100000.0..100000.0);
+        let y:f64 = rng.gen_range(-100000.0..100000.0);
+
+        let p = [x,y];
+        let expected = nearest(&p,&positions);
+        let actual = kdt.nearest_position(&p);
+
+        if (p.euclidean_distance(expected) - p.euclidean_distance(&actual.unwrap())) < 1. {
+            success += 1;
+        } else {
+            dbg!(expected,actual.unwrap(),p.euclidean_distance(expected),p.euclidean_distance(&actual.unwrap()));
+        }
+    }
+
+    println!("正答率 {}%",success as f64 / COUNT as f64 * 100.);
+}
+#[test]
+fn large_2d() {
+    const COUNT:usize = 1000;
+
+    let mut kdt = KDTree::<2,f64,()>::new();
+
+    let mut rng = rand::thread_rng();
+
+    let mut positions:Vec<[f64;2]> = vec![];
+
+    for _ in 0..100000 {
+        let mut pn = [0f64;2];
+
+        for i in 0..2 {
+            let mut p:f64 = rng.gen_range(-100000.0..100000.0);
+            let pi:u8 = rng.gen_range(0..10);
+
+            if let Some(last) = positions.last() {
+                if pi == 0 {
+                    p = last[i];
+                }
+            }
+
+            pn[i] = p;
+        }
+
+        positions.push(pn.clone());
+        kdt.insert(pn,());
+    }
+
+    for _ in 0..COUNT {
+        let x:f64 = rng.gen_range(-100000.0..100000.0);
+        let y:f64 = rng.gen_range(-100000.0..100000.0);
+
+        let p = [x,y];
+        let _ = kdt.nearest_position(&p);
+    }
+}
+#[test]
+fn large_3d() {
+    const COUNT:usize = 1000;
+
+    let mut kdt = KDTree::<3,f64,()>::new();
+
+    let mut rng = rand::thread_rng();
+
+    let mut positions:Vec<[f64;3]> = vec![];
+
+    for _ in 0..100000 {
+        let mut pn = [0f64;3];
+
+        for i in 0..3 {
+            let mut p:f64 = rng.gen_range(-100000.0..100000.0);
+            let pi:u8 = rng.gen_range(0..10);
+
+            if let Some(last) = positions.last() {
+                if pi == 0 {
+                    p = last[i];
+                }
+            }
+
+            pn[i] = p;
+        }
+
+        positions.push(pn.clone());
+        kdt.insert(pn,());
+    }
+
+    for _ in 0..COUNT {
+        let x:f64 = rng.gen_range(-100000.0..100000.0);
+        let y:f64 = rng.gen_range(-100000.0..100000.0);
+        let z:f64 = rng.gen_range(-100000.0..100000.0);
+
+        let p = [x,y,z];
+        let _ = kdt.nearest_position(&p);
+    }
+}
+#[test]
+fn large_2d_unbalance() {
+    const COUNT:usize = 1000;
+
+    let mut kdt = KDTree::<2,f64,()>::new();
+
+    let mut rng = rand::thread_rng();
+
+    let mut positions:Vec<[f64;2]> = vec![];
+
+    for _ in 0..100000 {
+        let mut pn = [0f64;2];
+
+        for i in 0..2 {
+            let mut p:f64 = rng.gen_range(-100000.0..100000.0);
+            let pi:u8 = rng.gen_range(0..10);
+
+            if let Some(last) = positions.last() {
+                if pi == 0 {
+                    p = last[i];
+                }
+            }
+
+            pn[i] = p;
+        }
+
+        positions.push(pn.clone());
+    }
+
+    positions.sort_by(|a,b| a[0].partial_cmp(&b[0]).unwrap().then(a[1].partial_cmp(&b[1]).unwrap()) );
+
+    for p in &positions {
+        kdt.insert(p.clone(),());
+    }
+
+    for _ in 0..COUNT {
+        let x:f64 = rng.gen_range(-100000.0..100000.0);
+        let y:f64 = rng.gen_range(-100000.0..100000.0);
+
+        let p = [x,y];
+        let _ = kdt.nearest_position(&p);
+    }
 }
